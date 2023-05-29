61ba51ab563573c413febd803afc84d71aa86cd6
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-05-29 18:44:33 +1200

    base class final dengan turunanya dan menampilkan inputtan dari data

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -7,7 +7,7 @@ public:
 		cout << "Hallo saya function dari base class";
 	}
 };
-class derivedClass : public baseClass {
+class derivedClass : public baseClass {					//eror 1 karena menggunakan kata final yang dimana tidak dapat diturunkan lagi atau mematikan turunan
 public:
 	void perkenalan() {
 		cout << "Hallo saya function dari derived class";
